84e75343a0311389a6b2b8ae7c3b153a68a3d591
samuel Damilola <samfrexz69@gmail.com>
2023-08-21 01:10:09 -0700

    update files

--- models/place.py
+++ models/place.py
@@ -1,18 +1,53 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy import Table, Float, MetaData
+from sqlalchemy.orm import relationship
+from models.amenity import Amenity
+from models.review import Review
+from sqlalchemy.ext.declarative import declarative_base
+import models
 
+metadata = Base.metadata
 
-class Place(BaseModel):
-    """ A place to stay """
-    city_id = ""
-    user_id = ""
-    name = ""
-    description = ""
-    number_rooms = 0
-    number_bathrooms = 0
-    max_guest = 0
-    price_by_night = 0
-    latitude = 0.0
-    longitude = 0.0
-    amenity_ids = []
+class Place(BaseModel, Base):
+    """ 
+    A place to stay 
+    Attrs:
+        city_id: city id
+        user_id: user id
+        name: input
+        description: desc of str
+        number_rooms: room num in int
+        number_bathrooms: int
+        max_guest: max guest in int
+        price_by_night: price in int
+        latitude: lat in float
+        longitude: longitude in float
+        amenity_ids: amenity ids
+    """
+
+    __tablename__ = 'places'
+    city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
+    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+    name = Column(String(128), nullable=False)
+    description = Column(String(1024))
+    number_rooms = Column(Integer, nullable=False, default=0)
+    number_bathrooms = Column(Integer, nullable=False, default=0)
+    max_guest = Column(Integer, nullable=False, default=0)
+    price_by_night = Column(Integer, nullable=False, default=0)
+    latitude = Column(Float)
+    longitude = Column(Float)
+    amenity_ids = relationship('Amenity', secondary='place_amenity',
+                               viewonly=False)
+
+    place_amenity = Table('place_amenity', metadata,
+                          Column('place_id',
+                                 String(60),
+                                 ForeignKey('places.id'),
+                                 primary_key=True, nullable=False),
+                          Column('amenity_id',
+                                 String(60),
+                                 ForeignKey('amenities.id'),
+                                 primary_key=True, nullable=False))
