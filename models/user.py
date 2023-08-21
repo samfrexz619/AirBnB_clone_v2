6ba108971695ccb6a8773b2f40e99571451c647d
samuel Damilola <samfrexz69@gmail.com>
2023-08-21 08:34:30 -0700

    upd file

--- models/user.py
+++ models/user.py
@@ -22,5 +22,5 @@ class User(BaseModel, Base):
     password = Column(String(128), nullable=False)
     first_name = Column(String(128))
     last_name = Column(String(128))
-    places = relationship("Place", cascade='delete', backref="user")
     reviews = relationship("Review", cascade='delete', backref="user")
+    places = relationship('Place', backref='user', cascade='delete')
