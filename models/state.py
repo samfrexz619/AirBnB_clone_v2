ee64e9e145c7ff8a0790456494210233bc075464
samuel Damilola <samfrexz69@gmail.com>
2023-08-21 01:28:50 -0700

    update files

--- models/state.py
+++ models/state.py
@@ -1,8 +1,28 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from models.city import City
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+import models
+import os
 
+class State(BaseModel, Base):
+    """ 
+    State class 
+    Attr:
+        name: input
+    """
 
-class State(BaseModel):
-    """ State class """
-    name = ""
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
+    cities = relationship('City', backref='state', cascade='delete')
+
+    if ("HBNB_TYPE_STORAGE", None) is None:
+        @property
+        def cities(self):
+            res = []
+            for city in list(models.storage.all(City).values()):
+                if city.state_id == self.id:
+                    res.append(city)
+            return res
--- models/user.py
+++ models/user.py
@@ -1,11 +1,26 @@
 #!/usr/bin/python3
 """This module defines a class User"""
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
-class User(BaseModel):
-    """This class defines a user by various attributes"""
-    email = ''
-    password = ''
-    first_name = ''
-    last_name = ''
+class User(BaseModel, Base):
+    """
+    This class defines a user by various attributes
+    Attrs:
+        email: email ad
+        password: login password
+        first_name: first name
+        last_name: last_name
+    """
+
+    __tablename__ = 'users'
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128))
+    last_name = Column(String(128))
+    reviews = relationship('Review', backref='user',
+                           cascade='delete')
+    places = relationship('Place', backref='user',
+                          cascade='delete')
