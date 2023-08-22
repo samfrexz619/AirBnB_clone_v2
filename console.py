b32f7e58a433a423b9e8762c9506e3a21d68d2b8
BlessingOse <maryblessing633@gmail.com>
2023-08-22 10:17:46 -0700

    updated

--- console.py
+++ console.py
@@ -304,10 +304,10 @@ class HBNBCommand(cmd.Cmd):
         new_dict = storage.all()[key]
 
         # iterate through attr names and values
-        for i, att_name in enumerate(args):
+        for idx, att_name in enumerate(args):
             # block only runs on even iterations
-            if (i % 2 == 0):
-                att_val = args[i + 1]  # following item is value
+            if (idx % 2 == 0):
+                att_val = args[idx + 1]  # following item is value
                 if not att_name:  # check for att_name
                     print("** attribute name missing **")
                     return
