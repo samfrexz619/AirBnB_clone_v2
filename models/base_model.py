69b9b9acc75efdc09b59897b196aa5aaa78fc0fd
BlessingOse <maryblessing633@gmail.com>
2023-08-22 11:15:15 -0700

    upd

--- models/base_model.py
+++ models/base_model.py
@@ -1,5 +1,5 @@
-!/usr/bin/python3
-"""This module defines a base class for all models in our hbnb clone"""
+#!/usr/bin/python3
+"""This is the base model class for AirBnB"""
 from sqlalchemy.ext.declarative import declarative_base
 import uuid
 import models
@@ -11,21 +11,22 @@ Base = declarative_base()
 
 
 class BaseModel:
-    """A base class for all hbnb models"""
-    id = Column(String(60), primary_key=True, nullable=False)
-    created_at = Column(DateTime, nullable=False, default=datetime.utcnow())
-    updated_at = Column(DateTime, nullable=False, default=datetime.utcnow())
+    """This class will defines all common attributes/methods
+    for other classes
+    """
+    id = Column(String(60), unique=True, nullable=False, primary_key=True)
+    created_at = Column(DateTime, nullable=False, default=(datetime.utcnow()))
+    updated_at = Column(DateTime, nullable=False, default=(datetime.utcnow()))
 
     def __init__(self, *args, **kwargs):
-        """
-        Instatntiates a new model
+        """Instantiation of base model class
         Args:
-            args: args
-            kwargs: args
-        Attrs:
-            id: id generated
-            created_at: creation
-            updated_at: updated time
+            args: it won't be used
+            kwargs: arguments for the constructor of the BaseModel
+        Attributes:
+            id: unique id generated
+            created_at: creation date
+            updated_at: updated date
         """
         if kwargs:
             for key, value in kwargs.items():
@@ -44,7 +45,10 @@ class BaseModel:
             self.created_at = self.updated_at = datetime.now()
 
     def __str__(self):
-        """Returns a string representation """
+        """returns a string
+        Return:
+            returns a string of class name, id, and dictionary
+        """
         return "[{}] ({}) {}".format(
             type(self).__name__, self.id, self.__dict__)
 
@@ -54,13 +58,17 @@ class BaseModel:
         return self.__str__()
 
     def save(self):
-        """Updates updated_at with current time """
+        """updates the public instance attribute updated_at to current
+        """
         self.updated_at = datetime.now()
         models.storage.new(self)
         models.storage.save()
 
     def to_dict(self):
-        """Convert instance into dict """
+        """creates dictionary of the class  and returns
+        Return:
+            returns a dictionary of all the key values in __dict__
+        """
         my_dict = dict(self.__dict__)
         my_dict["__class__"] = str(type(self).__name__)
         my_dict["created_at"] = self.created_at.isoformat()
@@ -70,4 +78,6 @@ class BaseModel:
         return my_dict
 
     def delete(self):
+        """ delete object
+        """
         models.storage.delete(self)
