8bf3af4de92ec2ff70121da4045b2b1b3532e74d
BlessingOse <maryblessing633@gmail.com>
2023-08-22 10:33:38 -0700

    updated

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -145,27 +145,27 @@ class TestAmenity(unittest.TestCase):
             self.assertEqual(amenity.name, "")
 
     def test_to_dict_creates_dict(self):
-        """test to_dict method creates a dictionary with proper attrs"""
-        am = Amenity()
-        print(am.__dict__)
-        new_d = am.to_dict()
+        """test to_dict method"""
+        amty = Amenity()
+        print(amty.__dict__)
+        new_d = amty.to_dict()
         self.assertEqual(type(new_d), dict)
         self.assertFalse("_sa_instance_state" in new_d)
-        for attr in am.__dict__:
+        for attr in amty.__dict__:
             if attr is not "_sa_instance_state":
                 self.assertTrue(attr in new_d)
         self.assertTrue("__class__" in new_d)
 
     def test_to_dict_values(self):
-        """test that values in dict returned from to_dict are correct"""
-        t_format = "%Y-%m-%dT%H:%M:%S.%f"
-        am = Amenity()
-        new_d = am.to_dict()
+        """test """
+        tm_format = "%Y-%m-%dT%H:%M:%S.%f"
+        amty = Amenity()
+        new_d = amty.to_dict()
         self.assertEqual(new_d["__class__"], "Amenity")
         self.assertEqual(type(new_d["created_at"]), str)
         self.assertEqual(type(new_d["updated_at"]), str)
-        self.assertEqual(new_d["created_at"], am.created_at.strftime(t_format))
-        self.assertEqual(new_d["updated_at"], am.updated_at.strftime(t_format))
+        self.assertEqual(new_d["created_at"], amty.created_at.strftime(tm_format))
+        self.assertEqual(new_d["updated_at"], amty.updated_at.strftime(tm_format))
 
     def test_str(self):
         """test that the str method has the correct output"""
