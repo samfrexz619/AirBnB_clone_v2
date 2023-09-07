e1c488da06693ed46d17062bb3853e6b5f82e68a
samuel Damilola <samfrexz69@gmail.com>
2023-09-07 02:42:39 -0700

    upd file

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,38 +1,30 @@
 #!/usr/bin/python3
-# Fabfile to distribute an archive to a web server.
+""" Script that distributes an archive to web server"""
+
+from fabric.api import local
+from fabric.operations import run, put, sudo
 import os.path
 from fabric.api import env
-from fabric.api import put
-from fabric.api import run
+env.hosts = ['52.86.133.108', '35.175.128.250']
 
-env.hosts = ["52.86.133.108", "35.175.128.250"]
 
 def do_deploy(archive_path):
-    """ Deploys web static """
+    ''' deploys web '''
 
-    if not os.path.exists(archive_path):
+    if (os.path.isfile(archive_path) is False):
         return False
+
     try:
-        name = archive_path.replace('/', ' ')
-        name = shlex.split(name)
-        name = name[-1]
-
-        wname = name.replace('.', ' ')
-        wname = shlex.split(wname)
-        wname = wname[0]
-
-        releases_path = "/data/web_static/releases/{}/".format(wname)
-        tmp_path = "/tmp/{}".format(name)
-
+        nconfig = archive_path.split("/")[-1]
+        ndir = ("/data/web_static/releases/" + nconfig.split(".")[0])
         put(archive_path, "/tmp/")
-        run("mkdir -p {}".format(releases_path))
-        run("tar -xzf {} -C {}".format(tmp_path, releases_path))
-        run("rm {}".format(tmp_path))
-        run("mv {}web_static/* {}".format(releases_path, releases_path))
-        run("rm -rf {}web_static".format(releases_path))
-        run("rm -rf /data/web_static/current")
-        run("ln -s {} /data/web_static/current".format(releases_path))
-        print("New version deployed!")
+        run("sudo mkdir -p {}".format(ndir))
+        run("sudo tar -xzf /tmp/{} -C {}".format(nconfig, ndir))
+        run("sudo rm /tmp/{}".format(nconfig))
+        run("sudo mv {}/web_static/* {}/".format(ndir, ndir))
+        run("sudo rm -rf {}/web_static".format(ndir))
+        run("sudo rm -rf /data/web_static/current")
+        run("sudo ln -s {} /data/web_static/current".format(ndir))
         return True
-    except:
+    except Exception:
         return False
